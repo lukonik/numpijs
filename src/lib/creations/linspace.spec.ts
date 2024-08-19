69be69b4d96afd3de4824ea6d4e012e7f0288b1e
luka onik <luka.onikadze@toptal.com>
2024-08-19 17:03:16 +0400

    updated tests

--- src/lib/creations/linspace.spec.ts
+++ src/lib/creations/linspace.spec.ts
@@ -2,21 +2,6 @@ import { array } from './array';
 import { linspace } from './linspace';
 
 describe('linspace', () => {
-  test('generates a linearly spaced array with positive step', () => {
-    const result = linspace({ start: 0, stop: 10, num: 5 });
-    expect(result).toEqual([0, 2.5, 5, 7.5, 10]);
-  });
-
-  test('generates a linearly spaced array with negative step', () => {
-    const result = linspace({ start: 10, stop: 0, num: 5 });
-    expect(result).toEqual([10, 7.5, 5, 2.5, 0]);
-  });
-
-  test('returns an array of the same value when start equals stop', () => {
-    const result = linspace({ start: 5, stop: 5, num: 4 });
-    expect(result).toEqual([5, 5, 5, 5]);
-  });
-
   test('throws an error if num is not a positive integer', () => {
     expect(() => linspace({ start: 0, stop: 10, num: 0 })).toThrow(
       'num must be a positive integer.'
