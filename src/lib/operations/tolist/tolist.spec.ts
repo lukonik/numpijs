30ea94ac1a1472e4eebc5ff6f87bc38227147f98
luka onik <luka.onikadze@toptal.com>
2024-08-20 17:06:17 +0400

    feat: add toList

---
+++ src/lib/operations/tolist/tolist.spec.ts
@@ -0,0 +1,46 @@
+import { NDArray } from 'src/lib/ndarray/ndarray';
+import { tolist } from './tolist';
+
+describe('toList', () => {
+  it('Check on 3D', () => {
+    const nd = new NDArray({
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+      shape: [2, 2, 4],
+    });
+    const result = tolist(nd);
+    const check = [
+      [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+      ],
+      [
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+      ],
+    ];
+    expect(result).toEqual(check);
+  });
+
+  it('Check on 2D', () => {
+    const nd = new NDArray({
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+      shape: [2, 6],
+    });
+    const result = tolist(nd);
+    const check = [
+      [1, 2, 3, 4, 5, 6],
+      [7, 8, 9, 10, 11, 12],
+    ];
+    expect(result).toEqual(check);
+  });
+
+  it('Check on 1D', () => {
+    const nd = new NDArray({
+      data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      shape: [10],
+    });
+    const result = tolist(nd);
+    const check = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    expect(result).toEqual(check);
+  });
+});
