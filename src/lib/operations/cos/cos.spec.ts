a3fdce2a287faa55b22670a8a51e38b613816d98
luka onik <luka.onikadze@toptal.com>
2024-08-21 15:27:18 +0400

    feat: add cos

---
+++ src/lib/operations/cos/cos.spec.ts
@@ -0,0 +1,21 @@
+import { array } from 'src/lib/creations/array';
+import { cos } from './cos';
+import { NDArray } from 'src/lib/ndarray/ndarray';
+import { stringified } from '../stringified/stringified';
+describe('Cos', () => {
+  it('cos test', () => {
+    const nd = array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    const result = nd.do(cos());
+    const check = new NDArray({
+      data: [
+        0.5403022766113281, -0.416146844625473, -0.9899924993515015,
+        -0.6536436080932617, 0.28366219997406006, 0.9601702690124512,
+        0.7539022564888, -0.1455000340938568, -0.9111302495002747,
+        -0.83907151222229,
+      ],
+      shape: nd.shape,
+      dtype: nd.dtype,
+    });
+    expect(result).toNDArrayEqual(check);
+  });
+});
