12c8499a438b8f1001ec31401a82843fca58ba60
luka onik <luka.onikadze@toptal.com>
2024-08-23 12:38:17 +0400

    docs: add max

--- src/lib/operations/max/max.spec.ts
+++ src/lib/operations/max/max.spec.ts
@@ -2,7 +2,7 @@ import { DataTypes } from 'src/lib/ndarray/data-types';
 import { NDArray } from 'src/lib/ndarray/ndarray';
 import { max } from './max';
 
-describe('Max', () => {
+describe('max', () => {
   let nd: NDArray;
   beforeEach(() => {
     nd = new NDArray({
@@ -11,14 +11,17 @@ describe('Max', () => {
       shape: [2, 3, 3],
     });
   });
-  it('Axis 1', () => {
-    const aggr = max(nd, {
-      axis: 0,
-    });
-    const check = new NDArray({
-      data: [5, 6, 10, 7, 8, 13, 11, 12, 14],
-      shape: [3, 3],
-    });
-    expect(aggr).toNDArrayEqual(check);
+  it('Axis null', () => {
+    expect(max(nd)).toBe(14);
+  });
+  it('Axis 1D', () => {
+    expect(max(nd, { axis: 1 })).toNDArrayEqual(
+      new NDArray({ data: [9, 10, 7, 11, 12, 14], shape: [2, 3] })
+    );
+  });
+  it('Axis 2D', () => {
+    expect(max(nd, { axis: 2 })).toNDArrayEqual(
+      new NDArray({ data: [3, 5, 10, 10, 13, 14], shape: [2, 3] })
+    );
   });
 });
--- src/lib/operations/max/max.ts
+++ src/lib/operations/max/max.ts
@@ -1,6 +1,24 @@
 import { aggregate } from 'src/lib/common/aggregate';
 import { NDArray } from 'src/lib/ndarray/ndarray';
 
+/**
+ * Returns the maximum value of the NDArray
+ * @example
+ * ```
+ * import from "numpijs"
+ * const nd = new NDArray({
+ * data: [1, 2, 3, 4],
+ * shape: [2, 2],
+ * })
+ * const max = max(nd) // 4
+ * ```
+ * @param nd NDArray
+ * @param options options
+ * @param options.axis axis along which the maximum value is to be found, if not provided it scans through all the values
+ * @param options.initial minimum value to start with
+ * @param options.where condition to filter the values
+ * @returns ndArray
+ */
 export function max(
   nd: NDArray,
   options?: {
